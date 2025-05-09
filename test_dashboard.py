857c1a60caaaffeb0e22acf6f963213eaa3fbe0f
unknown <bons6710hos@gmail.com>
2025-05-09 11:52:39 +0300

    dasboard login

---
+++ test_dashboard.py
@@ -0,0 +1,42 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import dotenv
+import os
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
+
+
+def test_Login():
+    driver = webdriver.Chrome()
+    driver.get("https://merchantapp-dashboard.arifpay.net/")
+    time.sleep(5)
+    # driver.implicitly_wait(10)
+    phone_number = driver.find_element(By.XPATH, "(//input[@id=':r0:'])[1]")
+    phone_number.send_keys(os.getenv("PHONE_NO"))
+    time.sleep(2)
+    password = driver.find_element(By.XPATH, "(//input[@id=':r1:'])[1]")
+    password.send_keys(os.getenv("PASSWORD"))
+    time.sleep(2)
+    
+    #handle the captcha
+    input("Please solve the CAPTCHA and press Enter to continue...")
+    # Wait for the user to solve the CAPTCHA
+    # After solving the CAPTCHA, you can proceed with the login
+    # Click the login button
+    login_button = driver.find_element(By.XPATH, "(//button[normalize-space()='Sign in'])[1]")
+    login_button.click()
+    time.sleep(5)
+    #assert
+    element = driver.find_element(By.XPATH, "(//h1[normalize-space()='Dashboard'])[1]")
+    assert element.is_displayed(), "Login failed, dashboard not displayed."
+    print("Login successful, dashboard displayed.")
+    
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
+    driver.execute_script("window.scrollTo(0, 0);")
+    time.sleep(5)
+    
+
+    driver.quit()
---
+++ test_transaction.py
@@ -0,0 +1,42 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import dotenv
+import os
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
+
+
+def test_Transaction():
+    driver = webdriver.Chrome()
+    driver.get("https://merchantapp-dashboard.arifpay.net/")
+    time.sleep(5)
+    driver.maximize_window()
+    # driver.implicitly_wait(10)
+    phone_number = driver.find_element(By.XPATH, "(//input[@id=':r0:'])[1]")
+    phone_number.send_keys(os.getenv("PHONE_NO"))
+    time.sleep(2)
+    password = driver.find_element(By.XPATH, "(//input[@id=':r1:'])[1]")
+    password.send_keys(os.getenv("PASSWORD"))
+    time.sleep(2)
+    
+    #handle the captcha
+    input("Please solve the CAPTCHA and press Enter to continue...")
+    # Wait for the user to solve the CAPTCHA
+    # After solving the CAPTCHA, you can proceed with the login
+    # Click the login button
+    login_button = driver.find_element(By.XPATH, "(//button[normalize-space()='Sign in'])[1]")
+    login_button.click()
+    time.sleep(5)
+    #assert
+    element = driver.find_element(By.XPATH, "(//h1[normalize-space()='Dashboard'])[1]")
+    assert element.is_displayed(), "Login failed, dashboard not displayed."
+    print("Login successful, dashboard displayed.")
+    
+    # Click on the Transactions tab
+    transactions_tab = driver.find_element(By.XPATH, "(//span[normalize-space()='Transactions'])[1]")
+    transactions_tab.click()
+    time.sleep(5)
+
+    driver.quit()
